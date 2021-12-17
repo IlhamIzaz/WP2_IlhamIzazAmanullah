689b53832e11eda7a14af250b2d7d1216ef7208f
IlhamIzaz <ilhamkyuubi@gmail.com>
2021-12-17 09:02:03 +0700

    first commit

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -1,9 +1,9 @@
 <?php
 class Contoh1 extends CI_Controller
 {
- public function index()
- {
- echo "<h1>Perkenalkan</h1>";
- echo"Nama saya Farah Adelia Putri Saya tinggal di daerah bekasi olahraga yang saya sukai adalah bersepeda";
- }
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Ilham Izaz Amanullah Saya tinggal di daerah Cikarang olahraga yang saya sukai adalah Basket";
+    }
 }
